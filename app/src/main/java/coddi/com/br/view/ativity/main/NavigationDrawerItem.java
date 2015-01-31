3688ce9867469a36c93e8cde348db34e582811a6
Bruno <docsbruno@gmail.com>
2015-01-31 16:36:41 -0200

    #teste

---
+++ app/src/main/java/coddi/com/br/view/ativity/main/NavigationDrawerItem.java
@@ -0,0 +1,40 @@
+package coddi.com.br.view.ativity.main;
+
+
+/**
+ * @author bruno
+ */
+public class NavigationDrawerItem {
+
+    private String titulo;
+    private boolean isSection;
+
+    public NavigationDrawerItem() {
+    }
+
+    public NavigationDrawerItem(String title) {
+        this.titulo = title;
+        this.isSection = false;
+    }
+
+    public NavigationDrawerItem(String titulo, boolean isSection) {
+        this.titulo = titulo;
+        this.isSection = isSection;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean isSection() {
+        return isSection;
+    }
+
+    public void setSection(boolean isSection) {
+        this.isSection = isSection;
+    }
+}
---
+++ app/src/main/java/coddi/com/br/view/ativity/main/NavigationDrawerItemAdapter.java
@@ -0,0 +1,79 @@
+package coddi.com.br.view.ativity.main;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import coddi.com.br.coddi.R;
+
+/**
+ * Created by Bruno on 31/01/2015.
+ */
+public class NavigationDrawerItemAdapter extends BaseAdapter {
+
+
+    private final LayoutInflater mInflater;
+    private Context context;
+    private List<NavigationDrawerItem> itens;
+
+    public NavigationDrawerItemAdapter(Context context, List<NavigationDrawerItem> itens) {
+        this.context = context;
+        this.itens = itens;
+        this.mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return itens.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return itens.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+
+        NavigationDrawerItem item = itens.get(position);
+
+        if (item.isSection()) {
+            view = buscarViewSection(item);
+        } else {
+            view = buscarViewItem(item);
+        }
+        return view;
+    }
+
+    private View buscarViewItem(NavigationDrawerItem item) {
+        View view = mInflater.inflate(R.layout.drawer_list_item, null);
+
+        TextView txtTitulo = (TextView) view.findViewById(R.id.txtTitulo);
+        txtTitulo.setText(item.getTitulo());
+
+        return view;
+    }
+
+    private View buscarViewSection(NavigationDrawerItem item) {
+
+        View view = mInflater.inflate(R.layout.drawer_section_item, null);
+        view.setClickable(false);
+        view.setSelected(false);
+
+        TextView txtTituloSection = (TextView) view.findViewById(R.id.txtTituloSection);
+        txtTituloSection.setText(item.getTitulo());
+
+        return view;
+    }
+}
